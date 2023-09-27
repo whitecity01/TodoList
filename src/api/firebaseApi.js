39b9ae98b44835f465d685dbbc8d11dd210f35bc
whitecity01 <kh081104@gmail.com>
2023-09-27 23:59:20 +0900

    [edit] 데이터 요청 방식 변경

--- src/api/firebaseApi.js
+++ src/api/firebaseApi.js
@@ -1,54 +1,56 @@
-import { ref, set, onValue, remove, update } from 'firebase/database';
-import db from './firebase';
 import axios from "axios";
 
 const databaseURL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
 
-const updateData = (callback) => {  //데이터 요청
-  onValue(ref(db), (snapshot) => {
-    const todoList = [];
-    if (snapshot.val() !== null) {
-      Object.entries(snapshot.val().todos).map((todo) => todoList.push(todo[1]));
-    }
-    callback(todoList);
-  });
-};
-
-const loadTodos = async (callback) => {
+const loadTodos = async () => {
   try {
-    const res = await axios.get(
-      `${databaseURL}/todos.json`
-    );
+    const res = await axios.get(`${databaseURL}/todos.json`);
     const todosData = res.data;
 
     if (todosData) {
       const todosArray = Object.values(todosData);
-      callback(todosArray);
+      return todosArray;
+    } else {
+      return [];
     }
   } catch (error) {
     console.error("Firebase 데이터 가져오기 에러:", error);
   }
 };
 
-
-const addData = (todo) => {  //데이터 추가
-  set(ref(db, `/todos/${todo.text}`), todo);
+const addData = async (todo) => {
+  try {
+    await axios.put(`${databaseURL}/todos/${todo.id}.json`, todo);
+  } catch (error) {
+    console.error("데이터 추가 에러:", error);
+  }
 };
 
-const editData = (todo) => { //데이터 수정(checked)
-  update(ref(db, `/todos/${todo.text}`), todo);
+const editData = async (todo) => {
+  try {
+    console.log(todo);
+    await axios.patch(`${databaseURL}/todos/${todo.id}.json`, {
+      done: !todo.done,
+    });
+  } catch (error) {
+    console.error("데이터 업데이트 에러:", error);
+  }
 };
 
-const removeData = (todo) => {  //데이터 삭제
-  remove(ref(db, `/todos/${todo.text}`));
+const removeData = async (todo) => {
+  try {
+    await axios.delete(`${databaseURL}/todos/${todo.id}.json`);
+    console.log("삭제 성공");
+  } catch (error) {
+    console.error("데이터 삭제 에러:", error);
+  }
 };
 
 const firebase = {
-  updateData: updateData,
   addData: addData,
   editData: editData,
   removeData: removeData,
-  loadTodos : loadTodos,
+  loadTodos: loadTodos,
 };
 
 export default firebase;
