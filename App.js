d94d59a421c9d0524473136161d5e24c5a28104f
whitecity01 <kh081104@gmail.com>
2023-09-27 19:36:08 +0900

    [Upload] App.js upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+import TodoTemplate from "./pages/TodoTemplate";
+//import { loader as todosLoader } from "./pages/TodoTemplate";
+
+const router = createBrowserRouter([
+  {
+    path: "/", //경로
+    element: <RootLayout />, //컴포넌트 요소
+    errorElement: <ErrorPage />, //에러 처리 컴포넌트
+    children: [  
+      { index: true, element: <HomePage /> }, //초기 index 페이지 설정 유무 및 컴포넌트
+      {
+        path: "todos",
+        element: <TodoTemplate />,
+        //loader: todosLoader,
+      },
+    ]
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
