50220ff1b7f9838422940c0a04f4a7bec3e3cab3
whitecity01 <kh081104@gmail.com>
2023-09-27 21:30:04 +0900

    [Add] loadTodos 추가

--- src/api/firebaseApi.js
+++ src/api/firebaseApi.js
@@ -1,5 +1,8 @@
 import { ref, set, onValue, remove, update } from 'firebase/database';
 import db from './firebase';
+import axios from "axios";
+
+const databaseURL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
 
 const updateData = (callback) => {  //데이터 요청
   onValue(ref(db), (snapshot) => {
@@ -10,6 +13,23 @@ const updateData = (callback) => {  //데이터 요청
     callback(todoList);
   });
 };
+
+const loadTodos = async (callback) => {
+  try {
+    const res = await axios.get(
+      `${databaseURL}/todos.json`
+    );
+    const todosData = res.data;
+
+    if (todosData) {
+      const todosArray = Object.values(todosData);
+      callback(todosArray);
+    }
+  } catch (error) {
+    console.error("Firebase 데이터 가져오기 에러:", error);
+  }
+};
+
 
 const addData = (todo) => {  //데이터 추가
   set(ref(db, `/todos/${todo.text}`), todo);
@@ -28,6 +48,7 @@ const firebase = {
   addData: addData,
   editData: editData,
   removeData: removeData,
+  loadTodos : loadTodos,
 };
 
 export default firebase;
