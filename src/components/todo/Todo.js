9e45365a71a85a01405cf59cc57a1efdc126ff51
whitecity01 <kh081104@gmail.com>
2023-09-28 00:27:34 +0900

    [edit] 코드 가독성 향상 및 필요없는 코드 삭제

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,64 +1,51 @@
 import { useCallback, useState } from "react";
 import TodoInsert from "./TodoInsert";
 import TodoList from "./TodoList";
-import styles from "../../styles/todo/TodoTemplate.module.scss";
 import firebase from "../../api/firebaseApi";
 
 const Todo = ({ getTodos }) => {
-  const [todos, setTodos] = useState(getTodos);
+  const [todos, setTodos] = useState( getTodos );
 
-  const onInsert = useCallback(
-    async (text) => {
-      //todo 추가
-      const newId = Date.now();
-      const todo = {
-        id: newId,
-        text,
-        done: false,
-      };
-      const newTodos = [...todos, todo];
-      setTodos(newTodos);
+  const onInsert = useCallback(async (text) => {
+    const newId = Date.now();
+    const todo = {
+      id: newId,
+      text,
+      done: false,
+    };
+    const newTodos = [...todos, todo];
+    setTodos(newTodos);
 
-      firebase.addData(todo);
-    },
-    [todos]
-  );
+    firebase.addData(todo);
+  },[todos]);
 
-  const onRemove = useCallback(
-    (id) => {
-      //todo 삭제
-      todos.forEach((todo) => {
-        if (todo.id === id) {
-          firebase.removeData(todo); // 해당하는 id의 todo 삭제
-        }
-      });
-      const newTodos = todos.filter((todo) => todo.id !== id);
-      setTodos(newTodos);
-    },
-    [todos]
-  );
+  const onRemove = useCallback((id) => {
+    todos.forEach((todo) => {
+      if (todo.id === id) {
+        firebase.removeData(todo);
+      }
+    });
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  },[todos]);
 
-  const onToggle = useCallback(
-    (id) => {    
-      const newTodos = todos.map((todo) => {
-        if (todo.id === id) {
-          const updatedTodo = { ...todo, done: !todo.done };
-          firebase.editData(todo);
-          return updatedTodo;
-        }
-        return todo;
-      });
-      setTodos(newTodos);
-    },
-    [todos]
-  );
+  const onToggle = useCallback((id) => {    
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        const updatedTodo = { ...todo, done: !todo.done };
+        firebase.editData(todo);
+        return updatedTodo;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  },[todos]);
 
   return (
-    <div className={styles.TodoTemplate}>
-      <h1 className={styles["app-title"]}>TodoList</h1>
+    <>
       <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-    </div>
+    </>
   );
 };
 
